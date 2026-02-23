1472f23bbcd56d27b324f26b142be1b09036b04a
JackPearsonAtUAL <j.pearson0220251@arts.ac.uk>
2026-02-23 09:49:40 +0000

    Finished the queue and tidied up the code

--- stacks_and_queues.py
+++ stacks_and_queues.py
@@ -16,14 +16,14 @@ class Stack:
     # Removes and returns the first (front) element in the stack
     def pop(self):
         if self.isEmpty:
-            print("Stack is empty.")
+            return "Stack is empty."
         else:
             return self.stack.pop()
     
     # Prints out the first (front) element in the stack
     def peek(self):
         if self.isEmpty:
-            print("Stack is empty.")
+            return "Stack is empty."
         else:
             return self.stack[-1]
         
@@ -45,24 +45,47 @@ class Queue:
     # Removes and returns the first (front) element from the queue
     def dequeue(self):
         if self.isEmpty:
-            print("Stack is empty.")
+            return "Queue is empty."
         else:
-            return self.stack.pop()
+            return self.queue.pop(0)
     
     # Prints out the first (front) element in the stack
     def peek(self):
         if self.isEmpty():
-            print("Stack is empty.")
+            return "Queue is empty."
         else:
-            return self.stack.pop()
+            return self.queue[0]
 
     # Bool check if the queue is empty or not    
     def isEmpty(self):
-        return len(self.stack) == 0
+        return len(self.queue) == 0
     
     # returns the size of the queue
     def size(self):
-        return len(self.stack)
+        return len(self.queue)
 
 myStack = Stack()
+myStack.push('A')
+myStack.push('B')
+myStack.push('C')
+
+print("Stack: ", myStack.stack)
+print("Pop: ", myStack.pop())
+print("Stack after Pop: ", myStack.stack)
+print("Peek: ", myStack.peek())
+print("isEmpty: ", myStack.isEmpty())
+print("Size: ", myStack.size())
+
+print("")
+
 myQueue = Queue()
+myQueue.enqueue('A')
+myQueue.enqueue('B')
+myQueue.enqueue('C')
+
+print("Queue: ", myQueue.queue)
+print("Enqueue: ", myQueue.dequeue())
+print("Queue after Pop: ", myQueue.queue)
+print("Peek: ", myQueue.peek())
+print("isEmpty: ", myQueue.isEmpty())
+print("Size: ", myQueue.size())
