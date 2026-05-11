e84328b7d2f9d52d7f8a8e5a5d8cb8fb1a9d87c6
JackPearsonAtUAL <j.pearson0220251@arts.ac.uk>
2026-05-11 13:03:36 +0100

    Code for task 3 has been started

--- assignment-task-3/graph.py
+++ assignment-task-3/graph.py
@@ -39,4 +39,43 @@ class Graph:
 						self.adjacency_matrix[vertex][vertex2] = 1
 
 	def walk(self, source, target):
-		return []
+		verts = list(self.vertices)
+		start = verts.index(source) # Holds the starting node
+		end = verts.index(target)  # holds the end node
+
+		dist = [float('inf')] * len(verts) # Holds the distances between the source and current vertex
+		prev = [None] * len(verts) # Holds the data on all previous vertices
+
+		dist[0] = 0 # dist[0] is referencing the starting vertex
+
+		visited = [False] * len(verts) # Boolean for whether the node has been visited yet
+
+		for x in range(len(verts)):
+			minDist = float('inf') # sets the smallest distance between start and next vertex
+			u = None # u is the next vertex
+			
+			for i in range(len(verts)):
+				# This comparison is used to determine the smallest distance
+				if not visited[i] and dist[i] < minDist:
+					minDist = dist[i] # Sets the new smallest distance
+					u = i # Sets the next vertex
+					print("Value of u:", u, "value of i:", i, "min distance =", minDist)
+				
+				# if there is no next node or we're at the target, end the loop
+				if u is None or u == end:
+					print("Breaking the loop. Current vertex:", verts[u]) # debug
+					print("Distances:", dist) # debug
+					break
+
+				visited[u] = True # updates the code so it knows the current vertex has been visited
+				print("Vertex visited:", verts[u]) # debug
+
+				for v in range(len(verts)):
+					# makes sure that the current iteration is valid and that v hasn't been visited
+					if self.adjacency_matrix[u][v] != 0 and not visited[v]:
+						alt = dist[u] + self.adjacency_matrix[u][v]			
+						if alt < dist[v]:
+							dist[v] = alt
+
+		return dist
+
